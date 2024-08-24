190d2c682ae4883305abd37d5185df95b62db07c
GiuseppeNovielli <giuseppe.novielli@hrcoffee.it>
2024-08-24 17:37:34 +0200

    improve better debug, methods

--- drf_fullclean/serializers.py
+++ drf_fullclean/serializers.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Giuseppe Novielli 2024 (Copyright) 
+https://www.github.com/giuseppenovielli/ 
+
+https://www.django-rest-framework.org/community/3.0-announcement/#differences-between-modelserializer-validation-and-modelform
+https://github.com/encode/django-rest-framework/discussions/7850#discussioncomment-8380135
+"""
 from rest_framework import serializers
 
 from django.conf import settings
@@ -12,27 +20,47 @@ def print_debug(message):
     drf_full_clean = getattr(settings, 'DRF_FULL_CLEAN', {})
     if not drf_full_clean.get('DEBUG', False):
         return
-    print(message)
+    print('\nDRF_FULL_CLEAN -> {}'.format(message))
+    
+def instance_to_dict(instance):
+    value_to_dict = None
+    try:
+        value_to_dict = model_to_dict(instance)
+    except:
+        try:
+            value_to_dict = instance.__dict__
+        except:
+            pass
+    return value_to_dict
+                    
 
 class FullCleanModelSerializer(serializers.ModelSerializer):
-    """
-    Giuseppe Novielli 2024 (Copyright) 
-    https://www.github.com/giuseppenovielli/ 
-    
-    https://www.django-rest-framework.org/community/3.0-announcement/#differences-between-modelserializer-validation-and-modelform
-    https://github.com/encode/django-rest-framework/discussions/7850#discussioncomment-8380135
-    """
     def is_valid(self, raise_exception=False, exclude=None, validate_unique=True, extra_include=None, *args, **kwargs):
-        print_debug('\nFullCleanModelSerializer is_valid -> {}'.format(self.Meta.model)) 
+        """
+        Invoke ModelSerializer.is_valid() and call Model.full_clean()
+
+        Args:
+            raise_exception (bool, optional): https://www.django-rest-framework.org/api-guide/serializers/#raising-an-exception-on-invalid-data Defaults to False.
+            exclude (list, optional): https://docs.djangoproject.com/en/3.2/ref/models/instances/#django.db.models.Model.full_clean Defaults to None.
+            validate_unique (bool, optional): https://docs.djangoproject.com/en/3.2/ref/models/instances/#django.db.models.Model.full_clean Defaults to True.
+            extra_include (dict, optional): https://github.com/giuseppenovielli/drf-fullclean/discussions/4 Defaults to None.
+
+        Returns:
+            bool: Return True if valid, False otherwise
+        """
+        print_debug('FullCleanModelSerializer is_valid -> {}'.format(self.Meta.model)) 
         
         is_valid = super().is_valid(raise_exception=raise_exception)
         if not is_valid:
             return is_valid
 
-        return self.is_valid_model(raise_exception=raise_exception, 
+        is_valid = self.is_valid_model(raise_exception=raise_exception, 
                                    exclude=exclude, validate_unique=validate_unique, 
                                    extra_include=extra_include, 
                                    *args, **kwargs)
+
+        print_debug('---------')
+        return is_valid
         
     
     def is_valid_model(self, raise_exception=False, exclude=None, validate_unique=True, extra_include=None, *args, **kwargs):
@@ -41,7 +69,7 @@ class FullCleanModelSerializer(serializers.ModelSerializer):
         
         obj = self.model_instance(self.Meta.model, self.validated_data, self.instance, self.partial, extra_include, **kwargs)
         
-        print('Instance to FullClean -> {}'.format(model_to_dict(obj)))
+        print('Instance to FullClean -> {}'.format(instance_to_dict(obj)))
         if obj:
             errors = self.model_validation(obj, exclude, validate_unique, extra_include, **kwargs)
         else:
@@ -56,34 +84,14 @@ class FullCleanModelSerializer(serializers.ModelSerializer):
     
     #INSTANCE
     def model_instance(self, model_class, validated_data, instance=None, partial=False, extra_include=None, **kwargs):
+        print_debug('validated_data -> {}'.format(validated_data))
         if not instance:
             return self.model_instance_create(model_class, validated_data, extra_include, **kwargs)
         return self.model_instance_update(model_class, validated_data, instance, partial, extra_include, **kwargs)
     
     
-    def model_instance_create(self, model_class, validated_data, extra_include=None, **kwargs):
-
-        try:
-            i = model_class(**validated_data)
-        except:
-            return
-        
-        #Add fields value that are not in validated_data
-        if extra_include:
-            for key,value in extra_include.items():
-                setattr(i, key, value)
-                
-        return i
-        
-    
-    
-    def model_instance_update(self, model_class, validated_data, instance, partial=False, extra_include=None, **kwargs):
-        try:
-            for field in instance._meta.fields:
-                if field.name not in validated_data:
-                    continue
-                setattr(instance, field.name, validated_data[field.name])
-        except:
+    def add_extra_include_to_instance(self, instance, extra_include, **kwargs):
+        if not instance:
             return
         
         #Add fields value that are not in validated_data
@@ -91,7 +99,41 @@ class FullCleanModelSerializer(serializers.ModelSerializer):
             for key,value in extra_include.items():
                 setattr(instance, key, value)
         return instance
+    
+    # CREATE
+    def create_instance(self, model_class, validated_data, **kwargs):
+        try:
+            return model_class(**validated_data)
+        except Exception as e:
+            print_debug('Create Instance EXCEPTION -> {}'.format(e))
+    
+    
+    def model_instance_create(self, model_class, validated_data, extra_include=None, **kwargs):
+        i = self.create_instance(model_class, validated_data)
+        i = self.add_extra_include_to_instance(i, extra_include)    
+        return i
         
+    
+    # UPDATE
+    def update_instance(self, instance, validated_data, partial=False, **kwargs):
+        if not instance:
+            return
+        
+        try:
+            for field in instance._meta.fields:
+                if field.name not in validated_data:
+                    continue
+                setattr(instance, field.name, validated_data[field.name])
+            return instance
+        except Exception as e:
+            print_debug('Update Instance EXCEPTION -> {}'.format(e))
+            
+        
+    
+    def model_instance_update(self, model_class, validated_data, instance, partial=False, extra_include=None, **kwargs):
+        i = self.update_instance(instance, validated_data, partial, **kwargs)
+        i = self.add_extra_include_to_instance(i, extra_include, **kwargs)
+        return i
     
     #
     
@@ -157,7 +199,7 @@ class FullCleanModelSerializer(serializers.ModelSerializer):
         l = []
         if extra_include:
             for key, value in extra_include.items():
-                l.append({key: model_to_dict(value)})
+                l.append({key: instance_to_dict(value)})
         print_debug('extra include -> {}'.format(l))
             
         object.full_clean(exclude=exclude, validate_unique=validate_unique)
